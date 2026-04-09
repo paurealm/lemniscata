125b26b14c2e37c29150bbee8f46a4e0e55d017b
paurealm <pauete.sb@gmail.com>
2026-04-09 17:55:48 +0200

    me he hecho vulnerable a inyecciones

--- backend/apis/sendMessageApi.js
+++ backend/apis/sendMessageApi.js
@@ -6,7 +6,6 @@ const processMessage = async (message) => {
         from: `"Páramos de Lemniscata" <${process.env.LEMNISCATA_API_MAIL_USERNAME}>`,
         to: "paurealm@lemniscata.net",
         subject: "[Lemniscata] Has recibido un mensaje nuevo :3",
-        text: "Te han dejado un nuevo mensaje",
         html: `<p><strong>Te han dejado un nuevo mensaje en los Páramos de Lemniscata:</strong><p><p>${message}</p>`, // HTML body
     });
 }
