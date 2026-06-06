40d6325cc058c783839645cffedf3c06cf6db5e2
paurealm <pauete.sb@gmail.com>
2026-06-06 20:53:27 +0200

    mira mamá no sé hacer un fetch

--- web/scripts/estaticos.js
+++ web/scripts/estaticos.js
@@ -2,18 +2,22 @@ const setupFiles = () => {
     const fileContainer = document.getElementById("file-list")
     if (!fileContainer) return;
 
-    fetch("https://webapi.lemniscata.net/static_lookup")
-        .then(response => response.body.files)
-        .then(files => {
-            for (let file of files) {
-                console.log(file);
-                
-                const fileEntry = document.createElement("p");
-                fileEntry.innerHTML = file;
-                fileContainer.appendChild(fileEntry)
+    fetch("https://webapi.lemniscata.net/static_lookup", {
+        method: "GET",
+        redirect: "follow"
+    })
+    .then(response => response.json().files)
+    .then(files => {
+        for (let file of files) {
+            console.log(file);
+            
+            const fileEntry = document.createElement("p");
+            fileEntry.innerHTML = file;
+            fileContainer.appendChild(fileEntry)
 
-            }
-        })
+        }
+    })
+    .catch(error => console.error(error))
         
 }
 
